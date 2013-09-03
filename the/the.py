410170d62b4340e4381a8c546aa15c303e79ebd1
wenjun.yan <mylastnameisyan@gmail.com>
2013-09-03 22:41:01 +0900

    inspect

--- the/the.py
+++ the/the.py
@@ -5,7 +5,7 @@ from world import World
 
 
 
-def what(var):
+def inspect(var):
     s = '<' + var.__class__.__name__ + '>'
     if hasattr(var, "__name__"):
         s += var.__name__
@@ -13,7 +13,7 @@ def what(var):
         s += str(var)
     return s
 
-def args_detail(args):
+def arginspect(args):
     ret = ",".join(map(lambda x: str(x), args[0]))
     for key, value in args[1].iteritems():
         ret += "," + str(key) + "=" + str(value)
@@ -55,7 +55,7 @@ class The(object):
         getattr(self, '_' + name)()
 
     def __str__(self):
-        return what(self.obj) + " " + self.message
+        return inspect(self.obj) + " " + self.message
 
     def __assert(self, stmt, msg):
         try:
@@ -96,100 +96,100 @@ class The(object):
 
     def _true(self):
         self.__check(self.obj is True,
-                     "{} is not True".format(what(self.obj)))
+                     "{} is not True".format(inspect(self.obj)))
 
     def _false(self):
         self.__check(self.obj is False,
-                     "{} is not False".format(what(self.obj)))
+                     "{} is not False".format(inspect(self.obj)))
 
     def _none(self):
         self.__check(self.obj is None,
-                     "{} is not None".format(what(self.obj)))
+                     "{} is not None".format(inspect(self.obj)))
 
     def _exist(self):
         self.__check(self.obj is not None,
-                     "{} is None".format(what(self.obj)))
+                     "{} is None".format(inspect(self.obj)))
     _exists = _exist
 
     def _ok(self):
         self.__check(self.obj,
-                     "{} is empty".format(what(self.obj)))
+                     "{} is empty".format(inspect(self.obj)))
     _yes = _ok
 
     def _empty(self):
         self.__check(not self.obj,
-                     "{} is not empty".format(what(self.obj)))
+                     "{} is not empty".format(inspect(self.obj)))
 
     # ------------- api matchers -----------------
 
     def equal(self, value):
         return self.__check(self.obj == value,
-                            what(self.obj) + " is not equal to " + what(value))
+                            inspect(self.obj) + " is not equal to " + inspect(value))
     equals = equal
 
     def a(self, tp):
         return self.__check(isinstance(self.obj, tp),
                             "{} is not an instance of the {}".
-                            format(what(self.obj), what(tp)))
+                            format(inspect(self.obj), inspect(tp)))
     an = a
 
     def Is(self, other):
         return self.__check(self.obj is other,
-                            "{} is NOT {}".format(what(self.obj), what(other)))
+                            "{} is NOT {}".format(inspect(self.obj), inspect(other)))
 
     def above(self, n):
         return self.__check(self.obj > n,
-                            what(self.obj) + " is not bigger than " + what(n))
+                            inspect(self.obj) + " is not bigger than " + inspect(n))
     def below(self, n):
         return self.__check(self.obj < n,
-                            what(self.obj) + " is not less than " + what(n))
+                            inspect(self.obj) + " is not less than " + inspect(n))
 
     def match(self, regex):
         return self.__check(re.search(regex, self.obj))
 
     def length(self, n):
         return self.__check(len(self.obj) == n,
-                            "the length of " + what(self.obj) +
-                            " is not " + what(n))
+                            "the length of " + inspect(self.obj) +
+                            " is not " + inspect(n))
     size = length
 
     def item(self, key, value):
         return self.__check((key in self.obj) and (self.obj[key] == value),
-                            "no such item {}: {}".format(key, what(value)))
+                            "no such item {}: {}".format(key, inspect(value)))
 
     def items(self, **kwargs):
         ret = True
         for key, value in kwargs.iteritems():
             ret = ret and (key in self.obj) and (self.obj[key] == value)
-        return self.__check(ret, what(self.obj) +
-                            " doesn't contain " + what(kwargs))
+        return self.__check(ret, inspect(self.obj) +
+                            " doesn't contain " + inspect(kwargs))
 
     def key(self, key):
         return self.__check((key in self.obj),
-                            what(self.obj) + " has no such key: " + key)
+                            inspect(self.obj) + " has no such key: " + key)
 
     def value(self, val):
         return self.__check(val in self.obj.values(),
-                            what(self.obj) + " has no such value: " + what(val))
+                            inspect(self.obj) + " has no such value: " + inspect(val))
 
     def keys(self, *args):
         keys = self.obj.keys()
         ret = all(map(lambda x: x in keys, args))
-        return self.__check(ret, what(self.obj) +
-                            " doesn't contain keys: " + what(args))
+        return self.__check(ret, inspect(self.obj) +
+                            " doesn't contain keys: " + inspect(args))
 
     def values(self, *args):
         values = self.obj.values()
         ret = all(map(lambda x: x in values, args))
-        return self.__check(ret, what(self.obj) +
-                            " doesn't contain values: " + what(args))
+        return self.__check(ret, inspect(self.obj) +
+                            " doesn't contain values: " + inspect(args))
 
     def property(self, key, value=None):
         ret = hasattr(self.obj, key)
         if value:
             ret = (getattr(self.obj, key) == value)
         return self.__check(ret,  "{} have no such property: {} => {}".
-                            format(what(self.obj), what(key), what(value)))
+                            format(inspect(self.obj), inspect(key), inspect(value)))
     attr = attribute = property
 
     # def properties(self, *args, **kwargs):
@@ -203,13 +203,13 @@ class The(object):
     def include(self, item):
         return self.__check(item in self.obj,
                             "{} does not include {}".
-                            format(what(self.obj), what(item)))
+                            format(inspect(self.obj), inspect(item)))
     contains = contain = includes = include
 
     def within(self, items):
         return self.__check(self.obj in items,
                             "{} is not in {}".
-                            format(what(self.obj), what(items)))
+                            format(inspect(self.obj), inspect(items)))
     In = within
 
 
@@ -222,17 +222,17 @@ class The(object):
             ret = self.obj(*self.args[0], **self.args[1])
         except Exception as e:
             self.__check(False, "{} when called by {} throws an exception: {}".
-                         format(what(self.obj), args_detail(self.args), e))
+                         format(inspect(self.obj), arginspect(self.args), e))
         else:
             self.__check(ret == res, "{} when called by {} is equal to {} not {}".
-                         format(what(self.obj), args_detail(self.args),
-                                what(ret), what(res)))
+                         format(inspect(self.obj), arginspect(self.args),
+                                inspect(ret), inspect(res)))
         return self
 
     def respond_to(self, fn):
         return self.__check(self.__check(hasattr(self.obj, fn)) and
                             self.__check(callable(getattr(self.obj, fn))),
-                            what(self.obj) + " not respond to " + what(fn))
+                            inspect(self.obj) + " not respond to " + inspect(fn))
     method = respond_to
 
     def throw(self, regex=None, tp=Exception):
@@ -242,10 +242,10 @@ class The(object):
             if regex:
                 self.__check(re.search(regex, e.message),
                              "{} when called by {} throws <{} {}> not <{} {}>".
-                             format(what(self.obj), args_detail(self.args),
+                             format(inspect(self.obj), arginspect(self.args),
                                     e.__class__.__name__, e.message,
                                     tp.__name__, regex))
         else:
             self.__check(False, '{} when called by {} No exception throws!'.
-                         format(what(self.obj, args_detail(self.args))))
+                         format(inspect(self.obj, arginspect(self.args))))
         return self
